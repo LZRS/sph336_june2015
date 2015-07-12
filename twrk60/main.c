62bef14a3ba5ce88ee3565aacc4ec11e17d02eb8
LZRS <LZRS@users.noreply.github.com>
2015-07-12 23:37:36 +0300

    Update main.c
    accepts decimal, converts to binary and displays binary using leds

--- twrk60/main.c
+++ twrk60/main.c
@@ -7,17 +7,13 @@
 
 #include "main.h"
 
-int main(void){	
-	//whether to blink LED2
+int main(void)
+	{
 	//uint8_t blink=1;
-	
-
 	//initialize system
 	SystemInit();
-
 	//initialize UART5 with 8-N-1 settings, 57600 baudrate
 	init_uart(UART5_BASE_PTR,periph_clk_khz,57600);
-
 	//clear all interrupts and enable interrupts
 	nvic->ICPR[2] |= 1 << (87 & 0x1F); //Clear Pending Interrupts
 	nvic->ISER[2] |= 1 << (87 & 0x1F); //Enable Interrupts
@@ -31,19 +27,27 @@ int main(void){
 		//use polling method to echo back data when available
 		if(data_available()){ 
 			byte = uart_read();
-			if(byte==0xD){ 
+				if(byte==0xD)
+				{ 
 				puts((uint8_t *)"\r\n"); //send new line character
-			//0 to 9 ascii code in hexadecimal 
-			}else if ((byte >= 0x30) && (byte <= 0x39)) 
-			{byte = byte - 0x30; display(byte);
-			
-			//small case letters a to f ascii code in hexadecimal
-			}else if ((byte >= 0x61) && (byte <= 0x66)){
+				//0 to 9 ascii code in hexadecimal 
+				}	
+				else if ((byte >= 0x30) && (byte <= 0x39)) 
+				{
+				byte = byte - 0x30; display(byte);
+				//small case letters a to f ascii code in hexadecimal
+				}	
+				else if ((byte >= 0x61) && (byte <= 0x66))
+				{
 				byte = byte - 0x57; display(byte);
-				}else if((byte >= 0x41) && (byte <= 0x46)){
+				}
+				else if((byte >= 0x41) && (byte <= 0x46))
+				{
 					byte = byte - 0x37; display(byte);
-				}else{
-				
+				}
+				else
+				{
+				display(byte);
 				}
 		}
 	}
@@ -54,11 +58,11 @@ int main(void){
 */
 void delay(void)
 {
-  volatile unsigned int i,j;
+  volatile unsigned int l,c;
 
-  for(i=0; i<1000; i++)
+  for(l=0; l<1000; l++)
   {
-	for(j=0; j<300; j++)
+	for(c=0; c<300; c++)
       __asm__("nop");
   }
 }
